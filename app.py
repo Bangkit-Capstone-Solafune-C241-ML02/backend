a9714cb7b13921671b002d9717138133c6c5db19
Abiya <aabbiiyyaa@gmail.com>
2024-05-16 00:18:19 +0700

    Add new route, /predict

--- app.py
+++ app.py
@@ -12,6 +12,27 @@ app = Flask(__name__)
 def home():
     return "App is running"
 
+@app.route('/predict', methods=['POST'])
+def predict_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    # Check if the file is a TIFF image
+    if file.filename == '' or not file.filename.endswith('.tif'):
+        return jsonify({'error': 'Invalid file format. Please provide a TIF image.'})
+
+    # try:
+    #     image = Image.open(file)
+    #     image_tensor = preprocess_image(image)
+    #     prediction = predict(image_tensor)
+    #     return jsonify({'prediction': prediction})
+    # except Exception as e:
+    #     return jsonify({'error': str(e)})
+
+    return jsonify({'message': 'Image received'})
+
 
 if __name__ == "__main__":
-    app.run(host=ip_address, port=5000)
+    app.run(host=ip_address, port=5000,debug=True)
